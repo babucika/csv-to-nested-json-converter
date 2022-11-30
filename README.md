07a8f1dff585b44758f212f00311f84706279bdf
eva kovacs <evak1@2checkout.com>
2022-11-30 13:37:18 +0100

    Create logic

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # csv-to-nested-json-converter
+
+To convert a csv file to neseted json do the following steps:
+
+1. Create a csv file in the following format. The column headers must be the nested keys concatenated by "__":
+
+   variablesToBeUpdated__id|expectedResult__statusCodeTest__testName|expectedResult__statusCodeTest__code|expectedResult__responseBodyTest__testName|expectedResult__responseBodyTest__body__code|expectedResult__responseBodyTest__body__detail|expectedResult__responseBodyTest__body__message
+   --- | --- | --- | --- |--- |--- |--- 
+   365|Status code if 'id' datatype is invalid|400|Response body if 'id' datatype is invalid|400|id must be of type string|id must be of type string
+
+2. Run the following command. The csv specified in the first argument will be converted to nested json. The nested json file will be created as specified in the second argument.
+```
+php convertCsvToJson.php path_to_csv_file path_to_json_file
+```
+
+3. If you open the created json file you should see something like:
+```javascript
+[
+    {
+        "variablesToBeUpdated": {
+            "id": 365
+        },
+        "expectedResult": {
+            "statusCodeTest": {
+                "testName": "Status code if 'id' datatype is invalid",
+                "code": 400
+            },
+            "responseBodyTest": {
+                "testName": "Response body if 'id' datatype is invalid",
+                "body": {
+                    "code": 400,
+                    "detail": "id must be of type string",
+                    "message": "id must be of type string"
+                }
+            }
+        }
+    }
+]
+```
--- src/CsvToJsonConverter.php
+++ src/CsvToJsonConverter.php
@@ -2,7 +2,71 @@
 
 namespace CsvConverter;
 
+use CsvConverter\Helper\CSVProcessor;
+
 class CsvToJsonConverter
 {
+    /**
+     * @var CSVProcessor
+     */
+    private CSVProcessor $CSVProcessor;
 
+    /**
+     * @param CSVProcessor $CSVProcessor
+     */
+    public function __construct(CSVProcessor $CSVProcessor)
+    {
+        $this->CSVProcessor = $CSVProcessor;
+    }
+
+    /**
+     * @param string $csvPath
+     * @param string $jsonPath
+     * @return void
+     */
+    public function convertCsvToJson(string $csvPath, string $jsonPath):void
+    {
+        $csvDataFlat = $this->CSVProcessor->getAssocArrayFromCSV($csvPath);
+        $csvDataNested = [];
+
+        foreach ($csvDataFlat as $index => $csvDataFlatRow)
+        {
+            $csvDataNested[$index] = [];
+            foreach ($csvDataFlatRow as $combinedKey => $value) {
+                $separateKeys = explode("__", $combinedKey);
+                $this->convertCombinedKeyToNested($csvDataNested[$index], $separateKeys, 0, $value);
+            }
+        }
+        $json = json_encode($csvDataNested, JSON_NUMERIC_CHECK|JSON_PRETTY_PRINT);
+
+        file_put_contents($jsonPath, $json);
+    }
+
+    /**
+     * @param array $resultArray
+     * @param array $separateKeys
+     * @param int $currentIndex
+     * @param mixed $value
+     * @return void
+     */
+    private function convertCombinedKeyToNested(array &$resultArray, array $separateKeys, int $currentIndex, $value):void
+    {
+        if ($currentIndex == count($separateKeys) - 1)
+        {
+            $resultArray[$separateKeys[$currentIndex]] = $value;
+        }
+        else
+        {
+            if (!isset($resultArray[$separateKeys[$currentIndex]]))
+            {
+                $resultArray[$separateKeys[$currentIndex]] = array();
+            }
+
+            $this->convertCombinedKeyToNested(
+                $resultArray[$separateKeys[$currentIndex]],
+                $separateKeys,
+                ++$currentIndex,
+                $value);
+        }
+    }
 }
